22f671ceef439de354f17e49c9ac4ee9d2674ebf
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-09 19:34:10 +0100

    customised admin for products

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categorie'
     name = models.CharField(max_length=254)
 
     def __str__(self):
