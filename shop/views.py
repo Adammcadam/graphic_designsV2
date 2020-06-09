77a68841fe633dc913e76247ed3bbd17ab81d831
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-09 20:02:02 +0100

    added shop page html & css

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    """ A view to show all products """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/shop.html', context)
+
+# def product(request):
+#     """ A view to show singular products """
+#     return render(request, 'shop/product.html', context)
