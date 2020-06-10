4f8d85188782092222d48936686137c1a4f17e0c
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-10 20:07:07 +0100

    added cart context

--- graphic_designs/settings.py
+++ graphic_designs/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages', # required by allauth DO NOT REMOVE
+                'cart.contexts.cart_contents',
             ],
         },
     },
