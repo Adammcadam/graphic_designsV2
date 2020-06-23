4a9febf11d6f865d508bd613326342955897c694
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-23 20:46:28 +0100

    created checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,60 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from shop.models import Product
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=16, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    address_line1 = models.CharField(max_length=80, null=False, blank=False)
+    address_line2 = models.CharField(max_length=80, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """ 
+        Generate a random order number using uuid
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ 
+        Account for items being added to update total
+        """
+        self.order_total = self.orderitems.aggregate(Sum('orderitem_total'))['orderitem_total__sum']
+    
+    def save(self, *args, **kwargs):
+        """ 
+        Override original save method to ensure order number is generated
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='orderitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    size = models.CharField(max_length=1, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    orderitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ 
+        Override original save method to set the order total
+        """
+        self.orderitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.product.name} on order {self.order.order_number}'
