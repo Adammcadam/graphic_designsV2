53d629c9053a604c99fa1e4ea8179abd3857dff1
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-24 20:07:18 +0100

    added context processor

--- graphic_designs/settings.py
+++ graphic_designs/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages', # required by allauth DO NOT REMOVE
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
             'builtins': [
