c9c4d99b224d9685980f76d9da7f587c607c2c31
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-27 15:43:15 +0100

    updated styles on stripe and checkout form

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -8,16 +8,17 @@
 var stripePublicKey = $('#stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
-var elements = stripe.elements();
+var elements = stripe.elements({
+    fonts: [
+        {
+        cssSrc: 'http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext'
+        }
+    ]
+});
 var style = {
     base: {
         color: '#b4babf',
-        fontFamily: 'Lato, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
+        fontFamily: 'Lato'
     },
     invalid: {
         color: '#dc3545',
