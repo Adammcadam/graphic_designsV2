a40bee5241b5f5f32f50da80833bf64237bf0f22
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-27 20:25:23 +0100

    added css and js to profile page plus forms and profile details

--- profiles/models.py
+++ profiles/models.py
@@ -8,12 +8,12 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     """ A user profile model """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country *", null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_city = models.CharField(max_length=40, null=True, blank=True)
     default_address_line1 = models.CharField(max_length=80, null=True, blank=True)
     default_address_line2 = models.CharField(max_length=80, null=True, blank=True)
+    default_city = models.CharField(max_length=40, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label="Country", null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
