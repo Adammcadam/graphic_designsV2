47fde1a7154c9e0990dc5b5f11ab43de400aae74
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-27 13:58:52 +0100

    added a loading overlay. Fixed a models error

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=False, blank=False)
     city = models.CharField(max_length=40, null=False, blank=False)
     address_line1 = models.CharField(max_length=80, null=False, blank=False)
-    address_line2 = models.CharField(max_length=80, null=False, blank=False)
+    address_line2 = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
