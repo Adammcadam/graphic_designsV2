086df69f5057c5a34c0a77ca2b2118acbe7f46c0
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-27 19:16:05 +0100

    added basic profile template, css, views and urls

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def profile(request):
+    """ Displays the users profile info """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
