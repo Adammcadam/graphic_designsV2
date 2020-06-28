714cd5281760fcdbd12fc1e2d6605447dc2a2c07
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-28 18:36:13 +0100

    various bug fixes for quoting

--- quoting/models.py
+++ quoting/models.py
@@ -1,13 +1,36 @@
+import uuid
 from django.db import models
 
 class CustomProduct(models.Model):
+    unique_id = models.CharField(max_length=32, null=False, editable=False, default=0)
     length = models.IntegerField(blank=False, null=False)
     width = models.IntegerField(blank=False, null=False)
     height = models.IntegerField(blank=False, null=False)
     description = models.CharField(max_length=254, blank=False, null=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
 
     def __str__(self):
-        return self.price
+        return self.description
+
+    def _generate_unique_id(self):
+        """
+        Generate a random, unique id using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def _update_price(self):
+        price = (self.length * self.width * self.height) / 5000
+        return price
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the unique id and price
+        if it hasn't been set already.
+        """
+        if not self.unique_id:
+            self.unique_id = self._generate_unique_id()
+        if not self.price:
+            self.price = self._update_price()
+        super().save(*args, **kwargs)
--- quoting/views.py
+++ quoting/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from .forms import CustomQuoteForm
-from .models import CustomProduct
 
 def custom_quote(request):
     """ A view to return the custom quote page """
@@ -10,13 +9,6 @@ def custom_quote(request):
     if request.method == 'POST':
         form = CustomQuoteForm(request.POST, request.FILES)
         if form.is_valid():
-            custom_product = CustomProduct
-            length = form.cleaned_data['length']
-            width = form.cleaned_data['width']
-            height = form.cleaned_data['height']
-            total_price = ((length * width * height)/5000)
-            custom_product.price = total_price
-            print(total_price)
             form.save()
             messages.success(request, 'Successfully sent form. We have recieved your request and will in touch in due course')
             return redirect('home')
@@ -28,6 +20,5 @@ def custom_quote(request):
     template = 'quoting/quoting.html'
     context = {
         'form': form,
-        'total_price': total_price
     }
     return render(request, template, context)
