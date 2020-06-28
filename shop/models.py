3d9a5e9f3cd0bfc645aa4677c49fee528391e8e1
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-28 21:12:14 +0100

    removed unnecessary field from model

--- shop/models.py
+++ shop/models.py
@@ -15,7 +15,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
 
