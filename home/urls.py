a00256e55dd89f57c28fec1191963880edd753b4
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-28 14:43:41 +0100

    created our work page addeadded html and css

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('our-work', views.our_work, name='work'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     """ A view to return index page """
     return render(request, 'home/index.html')
+
+def our_work(request):
+    """ A view to return the work page to showcase designs """
+    return render(request, 'home/work.html')
