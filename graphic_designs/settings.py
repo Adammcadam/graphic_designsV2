82f9244b4ef93934961b09073bc8a7ba85b4d5b5
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-29 22:03:48 +0100

    added missing trailing slash to satic and media files

--- graphic_designs/settings.py
+++ graphic_designs/settings.py
@@ -208,5 +208,5 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION  = 'media'
 
     # override the media and static files for production 
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
